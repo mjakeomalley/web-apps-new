1e924373f525af54ce1e44158e1e6728f5607af0
Jake O'Malley <jake.omalley@kellogg.northwestern.edu>
2023-05-07 20:47:22 +0000

    1:57 into class

---
+++ app/controllers/tacos_controller.rb
@@ -0,0 +1,7 @@
+class TacosController < ApplicationController
+  def index
+    #html = "<h1>Tacos</h1><p>Let's eat delish tacos!</p>"
+    #render :inline => html 
+    render :template => "tacos/index"
+  end
+end
